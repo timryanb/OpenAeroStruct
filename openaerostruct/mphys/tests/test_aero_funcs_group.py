3b6d2eb3d2e663a5d09d288c4061abcc8a2811e4
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2023-03-20 10:57:47 -0400

    fixing viscous nan in test_aero_funcs_group.py

--- openaerostruct/mphys/tests/test_aero_funcs_group.py
+++ openaerostruct/mphys/tests/test_aero_funcs_group.py
@@ -1,5 +1,7 @@
 import unittest
 
+import openmdao.api as om
+
 from openaerostruct.mphys.aero_funcs_group import AeroFuncsGroup
 from openaerostruct.utils.testing import run_test, get_default_surfaces
 
@@ -7,14 +9,26 @@ from openaerostruct.utils.testing import run_test, get_default_surfaces
 class Test(unittest.TestCase):
     def test(self):
         surfaces = get_default_surfaces()
-        surfaces[0]["with_viscous"] = False
 
-        comp = AeroFuncsGroup(surfaces=[surfaces[0]], write_solution=False)
-        comp.set_input_defaults("wing.widths", val=[1.0, 1.0, 1.0])
-        comp.set_input_defaults("v", val=1.0)
-        comp.set_input_defaults("rho", val=1.0)
+        group = om.Group()
 
-        run_test(self, comp, complex_flag=True, method="cs")
+        ivc = group.add_subsystem("ivc", om.IndepVarComp())
+        ivc.add_output("alpha", val=1.0)
+        ivc.add_output("mach", val=0.6)
+        ivc.add_output("wing_widths", val=[1.0, 1.0, 1.0])
+        ivc.add_output("v", val=1.0)
+        ivc.add_output("rho", val=1.0)
+
+        group.add_subsystem("funcs", AeroFuncsGroup(surfaces=[surfaces[0]], write_solution=False))
+        group.promotes("funcs", [(f"{surfaces[0]['name']}.widths", f"{surfaces[0]['name']}_widths")])
+
+        group.connect("ivc.alpha", "funcs.aoa")
+        group.connect("ivc.mach", "funcs.mach")
+        group.connect("ivc.rho", "funcs.rho")
+        group.connect("ivc.v", "funcs.v")
+        group.connect("ivc.wing_widths", f"{surfaces[0]['name']}_widths")
+
+        run_test(self, group, complex_flag=True, method="cs")
 
 
 if __name__ == "__main__":
